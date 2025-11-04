752a1a312221e95030c9473f9b117b956084649c
JFK98 <silvio.villagra@gmail.com>
2025-11-03 22:19:38 -0300

    Update Usuario.js

--- backend/models/Usuario.js
+++ backend/models/Usuario.js
@@ -3,7 +3,7 @@ const { Schema, model } = require('mongoose');
 const UsuarioSchema = new Schema({
   nombre: { type: String, required: true },
   correo: { type: String, required: true, unique: true },
-  password: { type: String, required: true }, // en producción deberías encriptar
+  password: { type: String, required: true }, 
   rol: { type: String, enum: ['admin', 'encargado', 'dueño', 'cliente'], default: 'cliente' },
   telefono: { type: String }
 }, { timestamps: true });
